d4879b8ee94b604ababdfd3bdc9b7897e01608ad
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-01-11 13:52:47 +0100

    oberer Bereich
    Hier steht der Code für den oberen Bereich, der immer integriert wird.

---
+++ oben.php
@@ -0,0 +1,12 @@
+                    <tr>
+                        <th>
+                            <h2 class="obenlinks">Inovisco Direct</h2>
+                        </th>
+                        <th class="obenrechts">
+                            <h2>Angemeldet als: <?php echo $_SESSION['user']; ?><br>
+                            <img src="abbrechenkl.png" alt="abbrechen"> <a href="abmelden.php">Abmelden</a></h2>
+                        </th>
+                    </tr>
+                <tr>
+                    <td colspan="2">
+                        <span class="link"><a href="auswahl.php">zur&uuml;ck</a></span>
