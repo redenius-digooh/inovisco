0d75382143b8aafb4fe9395c0c049a9f8842b319
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-01-11 13:50:42 +0100

    Download
    Hier kann die Verfügbarkeit heruntergeladen werden.

---
+++ download.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+	<script language="JavaScript" type="text/javascript" src="campaign.js" charset="UTF-8" ></script>	
+        <title></title>
+    </head>
+    <body>
+        <form action="?login=1" method="post">
+            <table>                
+                <?php
+                require_once 'oben.php';
+                ?>
+                        <p>Verf&uuml;gbarkeit herunterladen.</p>
+                        <button type="submit" name="download" value="1">Download</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
