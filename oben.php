e180228a7bd3df27a9f96b52b66d0b1f9fb91889
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-01-12 14:38:32 +0100

    h2 auf h4 geändert

--- oben.php
+++ oben.php
@@ -3,8 +3,11 @@
                             <h2 class="obenlinks">Inovisco Direct</h2>
                         </th>
                         <th class="obenrechts">
-                            <h2>Angemeldet als: <?php echo $_SESSION['user']; ?><br>
-                            <img src="abbrechenkl.png" alt="abbrechen"> <a href="abmelden.php" class="weiss">Abmelden</a></h2>
+                            <h4>Angemeldet als: <?php echo $_SESSION['user']; ?>
+                                <br>
+                                <img src="abbrechenkl.png" alt="abbrechen"> 
+                                <a href="abmelden.php" class="weiss">Abmelden</a>
+                            </h4>
                         </th>
                     </tr>
                     <tr>
