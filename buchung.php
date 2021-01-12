ec5892a759bc84106e963f166ff032714eebaba2
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-01-12 14:36:23 +0100

    Bereich entfernt
    Bereich, der in details steht, entfernt.

--- buchung.php
+++ buchung.php
@@ -1,16 +1,14 @@
 <?php
 require_once 'angemeldet.php';
 
-if (isset($_FILES['datei'])) {
-    if ($_FILES['datei']['error'] == UPLOAD_ERR_OK) {
-        move_uploaded_file($_FILES['datei']['tmp_name'], "uploadfiles/" . 
-            $_FILES['datei']['name']);
-        $upload = 1;
-    }
-    else {
-        $upload = 2;
-    }
+if (isset($_FILES['datei']) && $_POST['neu'] == 1) {
+    $uploaded_dir = "./uploadfiles/";
+    $filename = $_FILES["datei"]["name"];
+    $path = $uploaded_dir . $filename;
+    move_uploaded_file($_FILES["datei"]["tmp_name"], $path);
+    $upload = 1;
 }
+
 ?>
 <!doctype html>
 <html lang="de">
@@ -19,7 +17,7 @@ if (isset($_FILES['datei'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
 	<script language="JavaScript" type="text/javascript" src="campaign.js" 
-        charset="UTF-8" ></script>	
+        charset="UTF-8"></script>	
         <title></title>
     </head>
     <body>
@@ -28,7 +26,8 @@ if (isset($_FILES['datei'])) {
 <?php
 require_once 'oben.php';
 ?>
-                <form action="buchung.php" method="post" enctype="multipart/form-data">
+                <form action="buchung.php" method="post" 
+                      enctype="multipart/form-data">
                     <table class="ohnerahmen">
                         <tr>
                             <td class="button" colspan="2">
@@ -38,54 +37,32 @@ require_once 'oben.php';
                         <tr>
                             <td>
 <?php
-require_once 'oben.php';
 if ($upload == 1) {
+    require_once 'details.php';
 ?>
-                            <p>Das Hochladen war erfolgreich. Alle Displays und Slots 
-                            sind verf&uuml;gbar, die Kampagne kann zur Prüfung 
-                            an Digooh gesendet werden!
-</p>
+                            </td>
+                        </tr>
 <?php
-    $client = new \GuzzleHttp\Client();
-    $response = $client->get(
-        'https://cms.digooh.com:8081/api/v1/companies',
-        [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $_SESSION['token_direct'],
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ]
-        ]
-    );if($response) echo"ja"; else echo"nee";
-    $body = $response->getBody();
-    print_r(json_decode((string) $body));
-}
-elseif ($upload == 2) {
-?>
-                            <p>Das Hochladen hat leider nicht geklappt, bitte 
-                            versuchen Sie es erneut.</p>
-<?php
-}
-else {
+} else {
 ?>
                             Bitte w&auml;hlen Sie eine Exceldatei von Ihrem 
                                 Rechner aus.
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="file" name="datei">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="button">
-                            <button type="submit" name="neu" value="1">hochladen
-                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="file" name="datei">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="button">
+                                <button type="submit" name="neu" value="1">
+                                    hochladen</button>
+                            </td>
+                        </tr>
 <?php
 }
 ?>
-                        </td>
-                    </tr>
                 </table>
             </form>
         </center>
