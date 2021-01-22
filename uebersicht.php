8f54272d056728b0a2ab70804719b6e38adc5ca6
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-01-22 16:56:44 +0100

    ADD: Sortierung und Filter

--- uebersicht.php
+++ uebersicht.php
@@ -5,19 +5,66 @@
 require_once 'db.php';
 require_once 'oben.php';
 
-$sql = "SELECT upload, inovisco, digooh FROM buchung WHERE user = '" . 
-        $_SESSION['user'] . "'";
+if ($_POST['was'] == 'upload') {
+    $was = 'AND upload = 1';
+}
+elseif ($_POST['was'] == 'noupload') {
+    $was = 'AND upload = 0';
+}
+elseif ($_POST['was'] == 'inovisco') {
+    $was = 'AND inovisco = 1';
+}
+elseif ($_POST['was'] == 'noupload') {
+    $was = 'AND digooh = 1';
+}
+else{}
+
+if ($_POST['wo'] == 'up') {
+    $was = ' ORDER BY datum';
+}
+elseif ($_POST['wo'] == 'down') {
+    $was = ' ORDER BY datum desc';
+}
+else {}
+
+$sql = "SELECT name, upload, inovisco, digooh FROM buchung WHERE user = '" . 
+        $_SESSION['user'] . "'" . $was;
 $result = $conn->query($sql);
 ?>
                             <table class="ohnerahmen">
                                 <tr>
                                     <td class="balken">
+                                        <form action="uebersicht.php" method="post">
                                         <table class="ohnerahmen">
+            <tr>
+                <td colspan="3">
+                    Alle Buchungen mit:
+                    <select name="was">
+                        <option value="-">---</option>
+                        <option value="upload">Upload Buchung</option>
+                        <option value="noupload">ohne Upload Buchung</option>
+                        <option value="inovisco">Pr&uuml;fung Inovisco</option>
+                        <option value="digooh">Pr&uuml;fung Digooh</option>
+                    </select>
+                </td>
+                <td colspan="5">
+                    Sortierung:
+                    <select name="wo">
+                        <option value="-">---</option>
+                        <option value="up">Erstelldatum aufsteigend</option>
+                        <option value="down">Erstelldatum absteigend</option>
+                    </select>
+                    &nbsp;&nbsp;&nbsp;
+                    <button type="submit" name="neu" value="1">
+                                Suchen</button>
+                </td>
+            </tr>
 <?php
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
 ?>
                             <tr>
+                                <td><?php echo $row['name']; ?></td>
                                 <td width="90px">
                                     <table class="table_klein">
                                         <tr>
@@ -30,7 +77,7 @@ if ($result->num_rows > 0) {
                                         </tr>
                                     </table>
                                 </td>
-                                <td width="80px">&#10132;&#10132;&#10132;</td>
+                                <td>&#10132;</td>
                                 <td width="90px">
                                     <table class="table_klein">
                                         <tr>
@@ -44,7 +91,7 @@ if ($result->num_rows > 0) {
                                         </tr>
                                     </table>
                                 </td>
-                                <td width="80px">&#10132;&#10132;&#10132;</td>
+                                <td>&#10132;</td>
                                 <td width="90px">
                                     <table class="table_klein">
                                         <tr>                                                
@@ -58,7 +105,7 @@ if ($result->num_rows > 0) {
                                         </tr>
                                     </table>
                                 </td>
-                                <td width="80px">&#10132;&#10132;&#10132;</td>
+                                <td>&#10132;</td>
                                 <td width="90px">
                                     <table class="table_klein">
                                         <tr>
@@ -73,25 +120,16 @@ if ($result->num_rows > 0) {
                                         </tr>
                                     </table>
                                 </td>
-                                <td>&#10132;&#10132;&#10132;&#10132;</td>
                             </tr>
 <?php
     }
 }
 ?>
                                         </table>
+                                        </form>
                                     </td>
                                 </tr>
                             </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="mittig" width: 33,33%>
-                            <form action="auswahl.php" method="post">
-                                <button type="submit" name="neu2" 
-                                    class="lila" value="1">
-                                Zur &Uuml;bersicht</button>
-                            </form>
                         </td>
                     </tr>
                 </table>
