2f9999e416ab60b6a79bd7658fd7f15453c096d3
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-01-25 12:02:01 +0100

    ADD: Abfrage ob eine Excel-Datei hochgeladen wird, Abfrage Spalten vorhanden

--- buchung.php
+++ buchung.php
@@ -3,15 +3,41 @@
  * Upload Excel file and display details.
  */
 require_once 'db.php';
+require __DIR__ .  '/vendor/autoload.php';
+
+$namefehlt = 0;
 
 if (isset($_FILES['datei']) && $_POST['neu'] == 1) {
-    $uploaded_dir = "./uploadfiles/";
-    $tag = date("Y-m-d-H-i-s");
-    $filename = $_FILES["datei"]["name"] . "_" . $tag;
-    $path = $uploaded_dir . $filename;
-    move_uploaded_file($_FILES["datei"]["tmp_name"], $path);
-    $upload = 1;
-    require_once 'import.php';
+    if ($_FILES['datei']["type"] == 
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+        
+        $tag = date("Y-m-d-H-i-s");
+        $filename = $_FILES["datei"]["name"] . "_" . $tag;        
+        $uploaded_dir = "./uploadfiles/";        
+        $path = $uploaded_dir . $filename;
+        move_uploaded_file($_FILES["datei"]["tmp_name"], $path);
+        $upload = 1;
+        try {        
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load(
+                    $uploaded_dir . $filename);
+            $data = array(1,$spreadsheet->getActiveSheet()->
+                    toArray(null,true,true,true)); 
+
+            $eins = $data[1][1]['A'];
+            $zwei = $data[1][1]['B'];
+            
+            if ($eins != 'Agentur' || $zwei != 'Name') {
+                $namefehlt = 1;
+                $upload = 0;
+            }
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        require_once 'import.php';
+    } else {
+        $wrongtyp = 1;
+    }
 }
 
 require_once 'oben.php';
@@ -34,6 +60,27 @@ if ($upload == 1) {
                 <form action="buchung.php" method="post" 
                   enctype="multipart/form-data">
                 <table class="ohnerahmen">
+    <?php
+    if ($wrongtyp) {
+    ?>
+                    <tr>
+                        <td class="fehler">
+                        Bitte w&auml;hlen Sie eine korrekte Datei aus!
+                        </td>
+                    </tr>
+    <?php
+    }
+    if ($namefehlt == 1) {
+    ?>
+                    <tr>
+                        <td class="fehler">
+                        Bitte sorgen Sie f&uuml;r eine korrekte Datei, 
+                        hier fehlen wichtige Spalten!
+                        </td>
+                    </tr>
+    <?php
+    }
+    ?>
                     <tr>
                         <td>
                         Bitte w&auml;hlen Sie eine Exceldatei von Ihrem 
@@ -53,17 +100,6 @@ if ($upload == 1) {
                     </tr>
                 </table>
                 </form>
-                <table class="ohnerahmen">
-                    <tr>
-                        <td class="mittig" width: 33,33%>
-                            <form action="auswahl.php" method="post">
-                                <button type="submit" name="neu2" 
-                                    class="lila" value="1">
-                                Zur &Uuml;bersicht</button>
-                            </form>
-                        </td>
-                    </tr>
-                </table>
 <?php
 }
 ?>
