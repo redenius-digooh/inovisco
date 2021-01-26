088e85997045602ad5d27f41d2d119be934222d4
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-01-26 09:57:35 +0100

    ADD: selected

--- uebersicht.php
+++ uebersicht.php
@@ -6,30 +6,46 @@ require_once 'db.php';
 require_once 'oben.php';
 
 if ($_POST['was'] == 'upload') {
-    $was = 'AND upload = 1';
+    $was = " AND upload = 1";
 }
 elseif ($_POST['was'] == 'noupload') {
-    $was = 'AND upload = 0';
+    $was = " AND upload = 0";
 }
 elseif ($_POST['was'] == 'inovisco') {
-    $was = 'AND inovisco = 1';
+    $was = " AND inovisco = 1";
 }
 elseif ($_POST['was'] == 'noupload') {
-    $was = 'AND digooh = 1';
+    $was = " AND digooh = 1";
 }
 else{}
 
+if (!empty($_POST['agent'])) {
+    $agent = " AND agentur = '" . $_POST['agent'] . "'";
+}
+
 if ($_POST['wo'] == 'up') {
-    $was = ' ORDER BY datum';
+    $order = " ORDER BY datum";
 }
 elseif ($_POST['wo'] == 'down') {
-    $was = ' ORDER BY datum desc';
+    $order = " ORDER BY datum desc";
 }
 else {}
 
+$sql = "SELECT agentur FROM buchung WHERE user"
+        . " = '" . 
+        $_SESSION['user'] . "'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        if (!in_array($row['agentur'], $agenturen)) {
+            $agenturen[] = $row['agentur'];
+        }
+    }
+}
+
 $sql = "SELECT name, agentur, upload, inovisco, digooh FROM buchung WHERE user"
         . " = '" . 
-        $_SESSION['user'] . "'" . $was;
+        $_SESSION['user'] . "'" . $was . $agent . $order;
 $result = $conn->query($sql);
 ?>
                         <table class="ohnerahmen">
@@ -45,28 +61,54 @@ $result = $conn->query($sql);
                                     <form action="uebersicht.php" method="post">
                                     <table class="ohnerahmen">
             <tr>
-                <td colspan="3">
-                    Alle Buchungen mit:
-                    <select name="was">
-                        <option value="-">---</option>
-                        <option value="upload">Upload Buchung</option>
-                        <option value="noupload">ohne Upload Buchung</option>
-                        <option value="inovisco">Pr&uuml;fung Inovisco</option>
-                        <option value="digooh">Pr&uuml;fung Digooh</option>
-                    </select>
-                </td>
-                <td colspan="6">
-                    Sortierung:
-                    <select name="wo">
-                        <option value="-">---</option>
-                        <option value="up">Erstelldatum aufsteigend</option>
-                        <option value="down">Erstelldatum absteigend</option>
-                    </select>
-                    &nbsp;&nbsp;&nbsp;
-                    <button type="submit" name="neu" value="1">
-                                Suchen</button>
-                </td>
-            </tr>
+                <td colspan="9">
+                    <table class="ohnerahmen">
+                        <tr>
+<td>
+    Alle Buchungen mit:
+    <select name="was">
+        <option value="">---</option>
+        <option value="upload" <?php if ($_POST['was'] == 'upload') 
+            echo "selected"; ?>>Upload Buchung</option>
+        <option value="noupload" <?php if ($_POST['was'] == 'noupload') 
+            echo "selected"; ?>>ohne Upload Buchung</option>
+        <option value="inovisco" <?php if ($_POST['was'] == 'inovisco') 
+            echo "selected"; ?>>Pr&uuml;fung Inovisco</option>
+        <option value="digooh" <?php if ($_POST['was'] == 'digooh') 
+            echo "selected"; ?>>Pr&uuml;fung Digooh</option>
+    </select>
+</td>
+<td>
+    Agentur:
+    <select name="agent">
+        <option value="">---</option>
+        <?php
+        foreach ($agenturen as $value) {
+        ?>
+    <option value="<?php echo $value; ?>" <?php if ($_POST['agent'] == $value) 
+            echo "selected"; ?>><?php echo $value; ?>
+    </option>
+        <?php } ?>
+    </select>
+</td>
+<td>
+    Sortierung:
+    <select name="wo">
+        <option value="">---</option>
+        <option value="up" <?php if ($_POST['wo'] == 'up') 
+            echo "selected"; ?>>Erstelldatum aufsteigend</option>
+        <option value="down" <?php if ($_POST['wo'] == 'down') 
+            echo "selected"; ?>>Erstelldatum absteigend</option>
+    </select>
+</td>
+<td>
+    <button type="submit" name="neu" value="1">
+                Suchen</button>
+</td>
+        </tr>
+    </table>
+</td>
+                            </tr>
 <?php
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
