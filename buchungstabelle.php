40bee4a26ceb12e9a4dba40cbb37d7db96b9077e
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-02-12 10:16:30 +0100

    ADD: neue Tabelle playerbuchung

--- buchungstabelle.php
+++ buchungstabelle.php
@@ -1,89 +1,96 @@
 <?php
-$sql = "SELECT id, display, kunde, name, start_date, end_date, play_times,"
-        . " deleted, agentur, angebot, inovisco, digooh, einfrieren FROM buchung"
+$sql = "SELECT id, kunde, name, start_date, end_date, play_times,"
+        . " agentur, angebot, inovisco, digooh, einfrieren, kunde, name"
+        . " FROM buchung"
         . " WHERE user = '" . $user . "'" . $an;
 $db_erg = mysqli_query($conn, $sql);
 
 while ($row = mysqli_fetch_array( $db_erg)) {
-    $display = $row['display'];
-    $id = $row['id'];
-    $alleid[] = $row['id'];
-    $start_date = $row['start_date'];
-    $end_date = $row['end_date'];
-    $play_times = $row['play_times'];
-    $name = $row['name'];
-    $kunde = $row['kunde'];
-    $deleted = $row['deleted'];
-    $agentur = $row['agentur'];
-    $angebot = $row['angebot'];
-    $digooh = $row['digooh'];
-    $inovisco = $row['inovisco'];
-    $digooh = $row['digooh'];
-    $einfrieren = $row['einfrieren'];
+    $sql2 = "SELECT id, players, deleted, lfsph FROM playerbuchung WHERE 1"
+        . $an;
+    $db_erg2 = mysqli_query($conn, $sql2);
+    while ($row2 = mysqli_fetch_array( $db_erg2)) {
+        $deleted = $row2['deleted'];
+        $lfsph = $row2['lfsph'];
+        $players = $row2['players'];
+        $playerid = $row2['id'];
+        
+        $id = $row['id'];
+        $alleid[] = $row['id'];
+        $start_date = $row['start_date'];
+        $end_date = $row['end_date'];
+        $play_times = $row['play_times'];
+        $agentur = $row['agentur'];
+        $angebot = $row['angebot'];
+        $digooh = $row['digooh'];
+        $inovisco = $row['inovisco'];
+        $digooh = $row['digooh'];
+        $einfrieren = $row['einfrieren'];
 
-    require __DIR__ .  '/vendor/autoload.php';
-    
-    $sql = "SELECT name FROM player WHERE id = " . $display;
-    $db = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_array( $db)) {
-        $displayname = $row['name'];
-    }
+        require __DIR__ .  '/vendor/autoload.php';
 
-    $client = new \GuzzleHttp\Client();
+        $sql = "SELECT name FROM player WHERE id = " . $players;
+        $db = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_array( $db)) {
+            $displayname = $row['name'];
+        }
 
-    if ($start_date != '' && $end_date >= date("Y-m-d")) {
-        try {
-            // get entries from least
-            $response = $client->post(
-                'https://cms.digooh.com:8081/api/v1/campaigns/least',
-                [
-                    'headers' => [
-                        'Authorization' => 'Bearer ' . $_SESSION['token_direct'],
-                        'Content-Type' => 'application/json',
-                        'Accept' => 'application/json',
-                    ],
-                    'json' => [
-                        'start_date' => $start_date,
-                        'end_date' => $end_date,
-                        'players' => $display
+        $client = new \GuzzleHttp\Client();
+
+        if ($start_date != '' && $end_date >= date("Y-m-d")) {
+            try {
+                // get entries from least
+                $response = $client->post(
+                    'https://cms.digooh.com:8081/api/v1/campaigns/least',
+                    [
+                        'headers' => [
+                            'Authorization' => 'Bearer ' . $_SESSION['token_direct'],
+                            'Content-Type' => 'application/json',
+                            'Accept' => 'application/json',
+                        ],
+                        'json' => [
+                            'start_date' => $start_date,
+                            'end_date' => $end_date,
+                            'players' => $players
+                        ]
                     ]
-                ]
-            );
-            $body = $response->getBody();
-            $data = json_decode((string) $body);
+                );
+                $body = $response->getBody();
+                $data = json_decode((string) $body);
 
-            foreach ($data as $key => $value) {
-                $lfsph = $value / 10;
+                foreach ($data as $key => $value) {
+                    $lfsph = $value / 10;
+                }
+
+            //    $restzeit = ($lfsph - $play_times);
+                $restzeit = ($lfsph);
+            }
+            catch (Exception $e) {
+                echo $e->getMessage();
             }
 
-        //    $restzeit = ($lfsph - $play_times);
-            $restzeit = ($lfsph);
-        }
-        catch (Exception $e) {
-            echo $e->getMessage();
+            if ($restzeit <= 0) {
+                $problem = 1;
+                $gesproblem = 1;
+                $probleme[] = $id;
+            }
+            elseif ($restzeit < $play_times) {
+                $problem = 1;
+                $gesproblem = 1;
+                $teilprobleme[] = $id;
+            }
+            else {
+                $problem = 0;
+            }
         }
 
-        if ($restzeit <= 0) {
-            $problem = 1;
-            $gesproblem = 1;
-            $probleme[] = $id;
-        }
-        elseif ($restzeit < $play_times) {
-            $problem = 1;
-            $gesproblem = 1;
-            $teilprobleme[] = $id;
-        }
-        else {
-            $problem = 0;
-        }
+        mysqli_set_charset($conn,"utf8");
+
+        $buchungen[] = array('agentur' => $agentur,
+            'players' => $players, 'problem' => $problem, 'start_date' =>
+            $start_date, 'end_date' => $end_date, 'id' => $id, 
+            'deleted' => $deleted, 'restzeit' => $restzeit, 'lfsph' => $lfsph,
+            'play_times' => $play_times, 'displayname' => $displayname,
+            'inovisco' => $inovisco, 'digooh' => $digooh);
     }
-    
-    mysqli_set_charset($conn,"utf8");
-    
-    $buchungen[] = array('agentur' => $agentur, 'name' => $name,
-        'display' => $display, 'problem' => $problem, 'start_date' =>
-        $start_date, 'end_date' => $end_date, 'id' => $id, 
-        'deleted' => $deleted, 'restzeit' => $restzeit, 'lfsph' => $lfsph,
-        'play_times' => $play_times, 'displayname' => $displayname,
-        'inovisco' => $inovisco, 'digooh' => $digooh);
 }
--- export.php
+++ export.php
@@ -10,97 +10,111 @@ require_once 'db.php';
 $user = $_POST['user'];
 $an = " AND angebot = " . $_POST['angebot'];
 
-$sql = "SELECT id, display, start_date, end_date, play_times FROM buchung"
+$sql = "SELECT id, start_date, end_date, play_times, kunde, name FROM buchung"
         . " WHERE user = '" . $_POST['user'] . "' AND angebot = " 
             . $_POST['angebot'];
 $db_erg = mysqli_query($conn, $sql);
 
-while ($row = mysqli_fetch_array( $db_erg)) {
-    $id = $row['id'];
-    $display = $row['display'];
-    $start_date = $row['start_date'];
-    $end_date = $row['end_date'];
-    $play_times = $row['play_times'];
+while ($row = mysqli_fetch_array($db_erg)) {
+    $sql2 = "SELECT id, players, deleted, lfsph FROM playerbuchung WHERE"
+        . " angebot = " . $_POST['angebot'];
+    $db_erg2 = mysqli_query($conn, $sql2);
+    while ($row2 = mysqli_fetch_array( $db_erg2)) {
+        $deleted = $row2['deleted'];
+        $lfsph = $row2['lfsph'];
+        $players = $row2['players'];
+        $playerid = $row2['id'];
+        
+        $id = $row['id'];
+        $start_date = $row['start_date'];
+        $end_date = $row['end_date'];
+        $play_times = $row['play_times'];
+        $kunde = $row['kunde'];
+        $name = $row['name'];
 
-    require_once __DIR__ .  '/vendor/autoload.php';
+        require_once __DIR__ .  '/vendor/autoload.php';
 
-    $client = new \GuzzleHttp\Client();
+        $client = new \GuzzleHttp\Client();
 
-    if ($start_date != '' && $end_date >= date("Y-m-d")) {
-        try {
-            // get entries from least
-            $response = $client->post(
-                'https://cms.digooh.com:8081/api/v1/campaigns/least',
-                [
-                    'headers' => [
-                        'Authorization' => 'Bearer ' . $_SESSION['token_direct'],
-                        'Content-Type' => 'application/json',
-                        'Accept' => 'application/json',
-                    ],
-                    'json' => [
-                        'start_date' => $start_date,
-                        'end_date' => $end_date,
-                        'players' => $display
+        if ($start_date != '' && $end_date >= date("Y-m-d")) {
+            try {
+                // get entries from least
+                $response = $client->post(
+                    'https://cms.digooh.com:8081/api/v1/campaigns/least',
+                    [
+                        'headers' => [
+                            'Authorization' => 'Bearer ' . $_SESSION['token_direct'],
+                            'Content-Type' => 'application/json',
+                            'Accept' => 'application/json',
+                        ],
+                        'json' => [
+                            'start_date' => $start_date,
+                            'end_date' => $end_date,
+                            'players' => $players
+                        ]
                     ]
-                ]
-            );
-            $body = $response->getBody();
-            $data = json_decode((string) $body);
+                );
+                $body = $response->getBody();
+                $data = json_decode((string) $body);
 
-            foreach ($data as $key => $value) {
-                $lfsph = $value / 10;
+                foreach ($data as $key => $value) {
+                    $lfsph = $value / 10;
+                }
+
+            //    $restzeit = ($lfsph - $play_times);
+                $restzeit = ($lfsph);
+            }
+            catch (Exception $e) {
+                echo $e->getMessage();
             }
 
-        //    $restzeit = ($lfsph - $play_times);
-            $restzeit = ($lfsph);
-        }
-        catch (Exception $e) {
-            echo $e->getMessage();
+            if ($restzeit <= 0) {
+                $problem = 1;
+                $gesproblem = 1;
+                $probleme[] = $id;
+            }
+            elseif ($restzeit < $play_times) {
+                $problem = 1;
+                $gesproblem = 1;
+                $teilprobleme[] = $id;
+            }
+            else {
+                $problem = 0;
+            }
         }
 
-        if ($restzeit <= 0) {
-            $problem = 1;
-            $gesproblem = 1;
-            $probleme[] = $id;
-        }
-        elseif ($restzeit < $play_times) {
-            $problem = 1;
-            $gesproblem = 1;
-            $teilprobleme[] = $id;
-        }
-        else {
-            $problem = 0;
-        }
+        $aufdb[] = array('id' => $id, 'playerid' => $playerid,
+            'players' => $players, 'problem' => $problem, 'start_date' =>
+            $start_date, 'end_date' => $end_date, 'lfsph' => $lfsph);
     }
 
-    $aufdb[] = array('id' => $id,
-        'display' => $display, 'problem' => $problem, 'start_date' =>
-        $start_date, 'end_date' => $end_date, 'lfsph' => $lfsph);
-}
-
-foreach ($aufdb as $key => $in) {
-    $sql = "UPDATE buchung SET "
-            . "export = 1,"
-            . "lfsph = " . $in['lfsph'] . ","
-            . "problem = " . $in['problem']
-            . " WHERE id = " . $in['id'];
-    $erg = mysqli_query($conn, $sql);
+    foreach ($aufdb as $key => $in) {
+        $sql = "UPDATE buchung SET "
+                . "export = 1"
+                . " WHERE id = " . $in['id'];
+        $erg = mysqli_query($conn, $sql);
+        $sql = "UPDATE playerbuchung SET "
+                . "lfsph = " . $in['lfsph'] . ","
+                . "problem = " . $in['problem']
+                . " WHERE id = " . $in['playerid'];
+        $erg = mysqli_query($conn, $sql);
+    }
 }
 
 require_once 'buchungstabelle.php';
 
 //set column headers
 $columnHeader = '';
-$columnHeader = "Displayname" . "\t" . "DisplayID" . "\t" . "verfügbare Einblendungen pro Stunde" . "\t";
+$columnHeader = "Displayname" . "\t" . "DisplayID" . "\t" . mb_convert_encoding("verfügbare Einblendungen pro Stunde", "UTF-8") . "\t";
 $setData = '';
-$upper = "Angebotsnummer" . "\t" . '"' . $angebot . '"' . "\n";
+$upper = "Angebotsnummer" . "\t" . '"' . $_POST['angebot'] . '"' . "\n";
 $upper .= "Kundenname" . "\t" . '"' . $kunde . '"' . "\n";
 $upper .= "Kampagne" . "\t" . '"' . $name . '"' . "\n\n";
 
 foreach ($buchungen as $key => $inhalt) {
     $rowData = '';
     $value = '"' . mb_convert_encoding($inhalt['displayname'], "UTF-8") . '"' . "\t";
-    $value .= '"' . $inhalt['display'] . '"' . "\t";
+    $value .= '"' . $inhalt['players'] . '"' . "\t";
     $value .= '"' . $inhalt['lfsph'] . '"' . "\t";
     $rowData .= $value;
     $setData .= trim($rowData) . "\n";  
