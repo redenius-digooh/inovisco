d18e89915000616536a1db605ef3854e46d6e9ef
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-02-15 15:26:59 +0100

    DELETED: js-Datei

--- oben.php
+++ oben.php
@@ -10,8 +10,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
         <script type='text/javascript' src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
-	<script language="JavaScript" type="text/javascript" src="campaign.js" 
-        charset="UTF-8"></script>	
         <title></title>
     </head>
     <body>
