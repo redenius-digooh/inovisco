1df8f554cf71a76a207dcec12f30d877b63ccbe5
redenius-digooh <76955130+redenius-digooh@users.noreply.github.com>
2021-03-12 11:57:36 +0100

    CHANGE: $angebotsid -> $angebot

--- import.php
+++ import.php
@@ -14,7 +14,7 @@ $j = 1;
 $sql = "SELECT MAX(angebot) AS id FROM buchung";
 $db_erg = mysqli_query($conn, $sql);
 while ($row = mysqli_fetch_array( $db_erg)) {
-    $angebotsid = $row['id'] + 1;
+    $angebot = $row['id'] + 1;
 }
 
 $foundInCells = array();
@@ -47,7 +47,7 @@ for ($row = 2; $row <= $highestRow; $row++){
     if ($j == 1) {
         $query = "INSERT INTO buchung (user, useremail, angebot, "
                 . "upload) VALUES ('" . $_SESSION['user'] . "', '" 
-                . $_SESSION['useremail'] . "', '" . $angebotsid . "', 1)";
+                . $_SESSION['useremail'] . "', '" . $angebot . "', 1)";
         $erg = mysqli_query($conn, $query);
     }
     
@@ -63,10 +63,10 @@ for ($row = 2; $row <= $highestRow; $row++){
 
     if ($_POST['was'] == 1) {
         $query = "INSERT INTO playerbuchung (players, custom_sn1, angebot) VALUES ('" 
-            . $players . "', '" . $player . "', '" . $angebotsid . "')";
+            . $players . "', '" . $player . "', '" . $angebot . "')";
     } else {
         $query = "INSERT INTO playerbuchung (players, custom_sn2, angebot) VALUES ('" 
-            . $players . "', '" . $player . "', '" . $angebotsid . "')";
+            . $players . "', '" . $player . "', '" . $angebot . "')";
     }
     $erg = mysqli_query($conn, $query);
     
